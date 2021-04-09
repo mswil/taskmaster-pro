cd0b5deb1df2b6702fbcd3290a66278508580dbb
Molly Wilson <molly_wilson1@outlook.com>
2021-04-09 15:36:50 -0500

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -181,16 +181,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
-    console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function (event) {
-    console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function (event) {
-    console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function (event) {
-    console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function (event) {
     // array to store the task data in
@@ -232,14 +234,13 @@ $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
   drop: function (event, ui) {
-    console.log("drop");
     ui.draggable.remove();
   },
   over: function (event, ui) {
-    console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function (event, ui) {
-    console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 /*End task handling*/
@@ -259,7 +260,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -299,7 +300,7 @@ loadTasks();
 
 //audits tasks every 30 minutes
 setInterval(function () {
-  $(".card .list-group-item").each(function(index, el) {
+  $(".card .list-group-item").each(function (index, el) {
     auditTask(el);
   });
 }, (1000 * 60) * 30);
